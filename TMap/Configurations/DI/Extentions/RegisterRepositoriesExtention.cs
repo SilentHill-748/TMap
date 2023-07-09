7f1df78199d2f7515312b46670597f57bd69575a
SilentHill <nikich213@gmail.com>
2023-07-09 13:56:35 +0500

    Add RegisterRepositories.cs.

---
+++ TMap/Configurations/DI/Extentions/RegisterRepositoriesExtention.cs
@@ -0,0 +1,16 @@
+﻿using SimpleInjector;
+
+using TMap.Domain.Abstractions.Repositories;
+using TMap.Persistence.Repositories;
+
+namespace TMap.Configurations.DI.Extentions;
+
+public static class RegisterRepositoriesExtention
+{
+    public static Container RegisterRepositories(this Container container)
+    {
+        container.Register<IMaterialRepository, MaterialRepository>();
+
+        return container;
+    }
+}
