2276461f80f56dbedf14c7dda5f7f9a5ec06d24c
SilentHill <nikich213@gmail.com>
2023-07-09 09:16:06 +0500

    Add TMapDbContext.cs.

---
+++ TMap.Persistence/TMapDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using TMap.Domain.Entities.Material;
+
+namespace TMap.Persistence;
+
+public class TMapDbContext : DbContext
+{
+    public TMapDbContext(DbContextOptions optopns) : base(optopns) { }
+
+    public DbSet<Material> Materials => Set<Material>();
+}
