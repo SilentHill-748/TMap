cf6974bc0b96b06c86a3bb4bca5ecc8fce54ce27
SilentHill <nikich213@gmail.com>
2023-07-09 12:08:39 +0500

    Add MaterialDAO.cs.

---
+++ TMap.Domain/DAO/Material/MaterialDAO.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+using TMap.Domain.Entities.Material;
+
+namespace TMap.Domain.DAO.Material;
+
+public class MaterialDAO
+{
+    [JsonProperty("Name")]
+    public string? Name { get; set; }
+
+    [JsonProperty("ThermalConductivity")]
+    public double ThermalConductivity { get; set; }
+
+    [JsonProperty("Density")]
+    public double Density { get; set; }
+
+    [JsonProperty("Humidity")]
+    public double Humidity { get; set; }
+
+    [JsonProperty("Type")]
+    [JsonConverter(typeof(StringEnumConverter))]
+    public MaterialType Type { get; set; }
+
+    [JsonProperty("Color")]
+    public string? ColorHexCode { get; set; }
+}
