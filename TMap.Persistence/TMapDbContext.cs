7492f1a4c76d685e2338457d5cc37ee00a2f5d12
SilentHill <nikich213@gmail.com>
2023-07-09 14:59:09 +0500

    Fix: override OnModelCreating in TMapDbContext.cs.

--- TMap.Persistence/TMapDbContext.cs
+++ TMap.Persistence/TMapDbContext.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.Reflection;
+
+using Microsoft.EntityFrameworkCore;
 
 using TMap.Domain.Entities.Material;
 
@@ -9,4 +11,9 @@ public class TMapDbContext : DbContext
     public TMapDbContext(DbContextOptions optopns) : base(optopns) { }
 
     public DbSet<Material> Materials => Set<Material>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
 }
