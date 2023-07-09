2e02548c3978408f35fb5c1e82591f7fed6acfed
SilentHill <nikich213@gmail.com>
2023-07-09 13:22:51 +0500

    Create IMaterialService.cs and MaterialService.cs.

---
+++ TMap.Application/Services/Material/MaterialService.cs
@@ -0,0 +1,38 @@
+﻿using TMap.Domain.Abstractions.Repositories;
+using TMap.Domain.Abstractions.Services.Material;
+using TMap.Domain.DTO.Material;
+using TMap.Domain.Entities.Material;
+
+namespace TMap.Application.Services.Material;
+
+public class MaterialService : IMaterialService
+{
+    private readonly IMaterialRepository _materialRepository;
+
+    public MaterialService(IMaterialRepository materialRepository)
+    {
+        ArgumentNullException.ThrowIfNull(materialRepository, nameof(materialRepository));
+
+        _materialRepository = materialRepository;
+    }
+
+    public IEnumerable<MaterialDTO> GetMaterialsByType(MaterialType type)
+    {
+        return _materialRepository.GetAllMaterialsByType(type);
+    }
+
+    public async Task CreateMaterialAsync(MaterialDTO materialDTO)
+    {
+        await _materialRepository.CreateMaterialAsync(materialDTO);
+    }
+
+    public async Task DeleteMaterialAsync(MaterialDTO materialDTO)
+    {
+        await _materialRepository.DeleteMaterialAsync(materialDTO);
+    }
+
+    public async Task UpdateMaterialAsync(MaterialDTO materialDTO)
+    {
+        await _materialRepository.UpdateMaterialAsync(materialDTO);
+    }
+}
---
+++ TMap.Domain/Abstractions/Services/Material/IMaterialService.cs
@@ -0,0 +1,35 @@
+﻿using TMap.Domain.DTO.Material;
+using TMap.Domain.Entities.Material;
+
+namespace TMap.Domain.Abstractions.Services.Material;
+
+public interface IMaterialService
+{
+    /// <summary>
+    ///     Get all materials from database by specified material type.
+    /// </summary>
+    /// <param name="type">The type of materials.</param>
+    /// <returns><see cref="MaterialDTO"/> collection.</returns>
+    IEnumerable<MaterialDTO> GetMaterialsByType(MaterialType type);
+
+    /// <summary>
+    ///     Add new material to database.
+    /// </summary>
+    /// <param name="materialDTO">The material data.</param>
+    /// <returns><see cref="Task"/></returns>
+    Task CreateMaterialAsync(MaterialDTO materialDTO);
+
+    /// <summary>
+    ///     Delete material from database.
+    /// </summary>
+    /// <param name="materialDTO">The material data.</param>
+    /// <returns><see cref="Task"/></returns>
+    Task DeleteMaterialAsync(MaterialDTO materialDTO);
+
+    /// <summary>
+    ///     Edit material data.
+    /// </summary>
+    /// <param name="materialDTO">The material data.</param>
+    /// <returns><see cref="Task"/></returns>
+    Task UpdateMaterialAsync(MaterialDTO materialDTO);
+}
