ca634b672eead4b4e5341339201898e83a1137d6
SilentHill <nikich213@gmail.com>
2023-07-09 11:41:30 +0500

    Create DataSeed.cs.

---
+++ TMap.Persistence/DataSeed.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.Json;
+
+using TMap.Domain.Entities.Material;
+
+namespace TMap.Persistence;
+
+public class DataSeed
+{
+    private readonly TMapDbContext _dbContext;
+
+    public DataSeed(TMapDbContext dbContext)
+    {
+        ArgumentNullException.ThrowIfNull(dbContext, nameof(dbContext));
+
+        _dbContext = dbContext;
+    }
+
+    public async Task SeedAsync()
+    {
+        var json = Path.Combine(Directory.GetCurrentDirectory(), "Materials.json");
+
+        var materials = JsonSerializer.Deserialize<List<Material>>(File.Open(json, FileMode.Open));
+
+        if (materials is null)
+            throw new Exception("Не удалось загрузить данные по базовым материалам!");
+
+        await _dbContext.Materials.AddRangeAsync(materials);
+        
+        _ = await _dbContext.SaveChangesAsync();
+    }
+}
