f425fdaa92934cfe14eaaa4d27835d3a5b818469
SilentHill <nikich213@gmail.com>
2023-07-10 22:21:02 +0500

    Implement new methods to MaterialService.cs.

--- TMap.Application/Services/Material/MaterialService.cs
+++ TMap.Application/Services/Material/MaterialService.cs
@@ -16,9 +16,41 @@ public class MaterialService : IMaterialService
         _materialRepository = materialRepository;
     }
 
-    public IEnumerable<MaterialDTO> GetMaterialsByType(MaterialType type)
+    public MaterialDTO GetMaterialByName(string name)
     {
-        return _materialRepository.GetAllMaterialsByType(type);
+        return _materialRepository.GetMaterialByName(name);
+    }
+
+    public IEnumerable<MaterialDTO> GetSoilMaterials()
+    {
+        return _materialRepository.GetAllMaterialsByType(MaterialType.Soil);
+    }
+
+    public IEnumerable<MaterialDTO> GetRoadMaterials()
+    {
+        var soilMaterials = _materialRepository.GetAllMaterialsByType(MaterialType.Soil);
+        var roadMaterials = _materialRepository.GetAllMaterialsByType(MaterialType.Road);
+        var soilAndRoadMaterials = _materialRepository.GetAllMaterialsByType(MaterialType.RoadAndSoil);
+
+        return soilMaterials
+            .Union(roadMaterials)
+            .Union(soilAndRoadMaterials)
+            .OrderBy(material => material.Name);
+    }
+
+    public IEnumerable<MaterialDTO> GetPipelineChannelInsulationMaterials()
+    {
+        return _materialRepository.GetAllMaterialsByType(MaterialType.ChannelInsulation);
+    }
+
+    public IEnumerable<MaterialDTO> GetPipelineInsulationMaterials()
+    {
+        return _materialRepository.GetAllMaterialsByType(MaterialType.Insulation);
+    }
+
+    public IEnumerable<MaterialDTO> GetPipeMaterials()
+    {
+        return _materialRepository.GetAllMaterialsByType(MaterialType.Pipeline);
     }
 
     public async Task CreateMaterialAsync(MaterialDTO materialDTO)
