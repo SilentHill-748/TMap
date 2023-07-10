d37b1827aee247954ececb5814348be8162cefd2
SilentHill <nikich213@gmail.com>
2023-07-10 22:07:07 +0500

    Modify IMaterialService.cs.

--- TMap.Domain/Abstractions/Services/Material/IMaterialService.cs
+++ TMap.Domain/Abstractions/Services/Material/IMaterialService.cs
@@ -1,20 +1,42 @@
 ﻿using TMap.Domain.DTO.Material;
-using TMap.Domain.Entities.Material;
 using TMap.Domain.Abstractions.Repositories;
 
 namespace TMap.Domain.Abstractions.Services.Material;
 
 public interface IMaterialService
 {
-    /// <summary>
-    ///     Get all materials from database by specified material type.
-    /// </summary>
-    /// <param name="type">The type of materials.</param>
-    /// <returns><see cref="MaterialDTO"/> collection.</returns>
-    IEnumerable<MaterialDTO> GetMaterialsByType(MaterialType type);
-
     /// <inheritdoc cref="IMaterialRepository.GetMaterialByName(string)"/>
     MaterialDTO GetMaterialByName(string name);
+
+    /// <summary>
+    ///     Get collection of soil materials for geological map.
+    /// </summary>
+    /// <returns>The collection of <see cref="MaterialDTO"/>.</returns>
+    IEnumerable<MaterialDTO> GetSoilMaterials();
+
+    /// <summary>
+    ///     Get collection of soil and road materials for geological road map.
+    /// </summary>
+    /// <returns>The collection of <see cref="MaterialDTO"/>.</returns>
+    IEnumerable<MaterialDTO> GetRoadMaterials();
+
+    /// <summary>
+    ///     Get collection of insulation materials for pipeline channel.
+    /// </summary>
+    /// <returns>The collection of <see cref="MaterialDTO"/>.</returns>
+    IEnumerable<MaterialDTO> GetPipelineChannelInsulationMaterials();
+
+    /// <summary>
+    ///     Get collection of insulation materials for pipes.
+    /// </summary>
+    /// <returns>The collection of <see cref="MaterialDTO"/>.</returns>
+    IEnumerable<MaterialDTO> GetPipelineInsulationMaterials();
+
+    /// <summary>
+    ///     Get collection of pipe materials for pipeline.
+    /// </summary>
+    /// <returns>The collection of <see cref="MaterialDTO"/>.</returns>
+    IEnumerable<MaterialDTO> GetPipeMaterials();
 
     /// <summary>
     ///     Add new material to database.
